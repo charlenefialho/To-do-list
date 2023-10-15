c474cfabf35440989d9b8d821e3464585c029888
charlenefialho <fialhocharlene@gmail.com>
2023-10-15 17:31:22 -0300

    feat: added username validation

--- src/main/java/com/charlenefialho/todolist/user/UserController.java
+++ src/main/java/com/charlenefialho/todolist/user/UserController.java
@@ -1,6 +1,9 @@
 package com.charlenefialho.todolist.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,8 +17,16 @@ public class UserController {
   private IUserRepository userRepository;
 
   @PostMapping("/")
-  public UserModel createUser(@RequestBody UserModel userModel){
+  public ResponseEntity createUser(@RequestBody UserModel userModel){
+    var user = this.userRepository.findByUsername(userModel.getUsername());
+
+    if(user != null){
+      System.out.println("Usuário já existe!!");
+      //mensagem de erro
+      //statusCode
+      return  ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe!!");
+    }
     var userCreated = this.userRepository.save(userModel);
-    return userCreated;
+    return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
   }
 }

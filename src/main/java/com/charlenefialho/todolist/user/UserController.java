3bc09a6c615a47aa92dde94dd46a98f94daa8c5f
charlenefialho <fialhocharlene@gmail.com>
2023-10-15 16:55:05 -0300

    feat: created IUserRepository and implementation

--- src/main/java/com/charlenefialho/todolist/user/UserController.java
+++ src/main/java/com/charlenefialho/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package com.charlenefialho.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+  @Autowired
+  private IUserRepository userRepository;
+
   @PostMapping("/")
-  public void createUser(@RequestBody UserModel userModel){
-    System.out.println(userModel.getName());
+  public UserModel createUser(@RequestBody UserModel userModel){
+    var userCreated = this.userRepository.save(userModel);
+    return userCreated;
   }
 }
